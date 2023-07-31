5d80bec4e749ec746070fc5a5fea5e3fd34b8ff5
Joshua Burt <112965527+JoshuaBurt1@users.noreply.github.com>
2023-07-31 13:25:55 -0400

    Delete JoshuaBurt_COMP1011Sec005_LabEx05 directory

--- JoshuaBurt_COMP1011Sec005_LabEx05/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.joshuaburt_comp1011sec005_labex05 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.joshuaburt_comp1011sec005_labex05 to javafx.fxml;
-    exports com.example.joshuaburt_comp1011sec005_labex05;
-}
